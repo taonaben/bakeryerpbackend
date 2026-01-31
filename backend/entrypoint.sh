0c8e0f95aee0b70de6cebfd240569d14f2925f1c
Taona Munikwa <69734394+taonaben@users.noreply.github.com>
2026-01-31 21:48:33 +0200

    fdghj

---
+++ backend/entrypoint.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+set -e
+
+echo "Running checks"
+python manage.py check
+
+echo "Running Django migrations..."
+python manage.py migrate --noinput
+
+
+
+echo "Collecting static files..."
+python manage.py collectstatic --noinput
+
+echo "Starting Gunicorn..."
+exec gunicorn core.wsgi:application --bind 0.0.0.0:8000
